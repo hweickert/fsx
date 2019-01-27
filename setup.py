d629ca54ee9f90e47bb549bc93f943aca6e744a3
Henry Weickert <henry.weickert>
2019-01-27 14:44:22 +0100

    Bumpversion v1.0.2 -> v1.0.3
    fix fstree dependency version number

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ description = r'''A low-level and easily mockable file system wrapper.'''
 setuptools.setup(
     name='fsx',
     packages = setuptools.find_packages(exclude=['tests*']),
-    version="1.0.2",
+    version="1.0.3",
     description=description,
     author = 'Henry Weickert',
     author_email = 'henryweickert@gmail.com',
     url = 'https://github.com/hweickert/fsx',
     keywords = [],
     entry_points={},
-    install_requires=['fstree==1.0.1']
+    install_requires=['fstree==1.0.2']
 )
