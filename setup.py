de30c7e67be0ebad45dce2bf4ec30a65edd1b32b
Henry Weickert <henry.weickert>
2022-02-04 11:46:25 +0100

    Bump version: "1.0.7" → "1.0.8"

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = r'''A low-level and easily mockable file system wrapper.'''
 setuptools.setup(
     name='fsx',
     packages = setuptools.find_packages(exclude=['tests*']),
-    version="1.0.7",
+    version="1.0.8",
     description=description,
     author = 'Henry Weickert',
     author_email = 'henryweickert@gmail.com',
